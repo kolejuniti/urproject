ec354d66646a49ac9b5406ac1ad3dbea14db51f7
faizulsoknan <mohdfaizul@uniti.edu.my>
2024-06-05 15:51:00 +0800

    update profile page

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -17,7 +17,7 @@
                             <label for="">Nama Penuh</label>
                         </div>
                         <div class="col-sm-9 col-md-9">
-                            <label for="">{{ $user->name }}</label>
+                            <label for=""></label>
                         </div>
                     </div>
                     <div class="row mb-2">
@@ -25,7 +25,7 @@
                             <label for="">No. Kad Pengenalan</label>
                         </div>
                         <div class="col-sm-9 col-md-9">
-                            <label for="">{{ $user->ic }}</label>
+                            <label for=""></label>
                         </div>
                     </div>
                     <div class="row mb-2">
@@ -33,13 +33,13 @@
                             <label for="">Agama</label>
                         </div>
                         <div class="col-md-3 col-sm-3">
-                            <label for="">{{ $user->religion }}</label>
+                            <label for=""></label>
                         </div>
                         <div class="col-md-3 col-sm-3">
                             <label for="">Bangsa</label>
                         </div>
                         <div class="col-md-3 col-sm-3">
-                            <label for="">{{ $user->nation }}</label>
+                            <label for=""></label>
                         </div>
                     </div>
                     <div class="row mb-2">
