cb091bda0327cd0fbd59bb44881c5b4522abb499
faizulsoknan <mohdfaizul@uniti.edu.my>
2024-06-21 17:23:46 +0800

    update login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        {{-- <div class="col-md-12">
             @if (session('success'))
                 <div class="alert alert-success">
                     {{ session('success') }}
@@ -66,18 +66,86 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                {{-- @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif --}}
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
+        </div> --}}
+
+        <section class="bg-light py-3 py-md-4">
+            <div class="container">
+            <div class="row justify-content-center">
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                <div class="card border border-light-subtle rounded-3 shadow-sm">
+                    <div class="card-body p-3 p-md-4 p-xl-5">
+                    <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Sign in to your account</h2>
+                    <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                        <div class="row gy-2 overflow-hidden">
+                        <div class="col-12">
+                            <div class="form-floating mb-3">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" value="{{ old('email') }}" placeholder="name@example.com" required autocomplete="email">
+                            
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                            <label for="email" class="form-label">Email</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating mb-3">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" value="" placeholder="Password" required autocomplete="current-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                            <label for="password" class="form-label">Password</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-flex gap-2 justify-content-between">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" name="rememberMe" id="rememberMe">
+                                <label class="form-check-label text-secondary" for="rememberMe">
+                                Keep me logged in
+                                </label>
+                            </div>
+                            {{-- <a href="#!" class="link-primary text-decoration-none">Forgot password?</a> --}}
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-grid my-3">
+                            <button class="btn btn-primary btn-lg" type="submit">Log in</button>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <p class="m-0 text-secondary text-center">Don't have an account? <a href="#!" class="link-primary text-decoration-none">Sign up</a></p>
+                        </div>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
     </div>
 </div>
 @endsection
