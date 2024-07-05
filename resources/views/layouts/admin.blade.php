4d3e7d20471699dd13c6c8aaca5ce612441dd39d
faizulsoknan <mohdfaizul@uniti.edu.my>
2024-07-06 00:00:14 +0800

    update permohonan to pelajar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,7 +48,7 @@
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Permohonan
+                                Pelajar
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
