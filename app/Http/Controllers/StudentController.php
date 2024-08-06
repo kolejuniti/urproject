98f9c26f8b33d62633faf906b029a4abcdbfd262
faizulsoknan <mohdfaizul@uniti.edu.my>
2024-08-06 14:53:37 +0800

    update source from tiktok

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -271,6 +271,8 @@ class StudentController extends Controller
             return 'whatsapp';
         } elseif (strpos($referrer, 'https://www.tiktok.com/') !== false) {
             return 'tiktok';
+        } elseif (strpos($referrer, 'https://edaftarkolej.uniticms.edu.my/student/about?ttclid=') !== false) {
+            return 'tiktok';
         } elseif (strpos($referrer, 'https://www.instagram.com/') !== false) {
             return 'instagram';
         } elseif (strpos($referrer, 'https://l.instagram.com/') !== false) {
