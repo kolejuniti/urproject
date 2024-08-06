65ff97b5542f5fb7764b16459b8443e19623b526
faizulsoknan <mohdfaizul@uniti.edu.my>
2024-08-06 11:37:19 +0800

    add loading spinner when page is loading

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,8 +15,46 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    
+    <style>
+        body {
+            position: relative;
+        }
+
+        #spinner {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(255, 255, 255, 0.8);
+            z-index: 1000;
+        }
+
+        .loader {
+            border: 16px solid #f3f3f3; /* Light grey */
+            border-top: 16px solid #3498db; /* Blue */
+            border-radius: 50%;
+            width: 120px;
+            height: 120px;
+            animation: spin 2s linear infinite;
+        }
+
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+        }
+    </style>
 </head>
 <body>
+    <!-- Spinner -->
+    <div id="spinner">
+        <div class="loader"></div>
+    </div>
+    
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
             <div class="container">
@@ -60,6 +98,7 @@
                             <ul class="dropdown-menu">
                                 <li>
                                     <a class="dropdown-item" href="{{ route('admin.studentlist') }}">Laporan Pelajar</a>
+                                    {{-- <a class="dropdown-item" href="{{ route('admin.summary') }}">Statistik Permohonan</a> --}}
                                 </li>
                             </ul>
                         </li>
@@ -230,5 +269,13 @@
             </div>
         </div>
     </div>
+    
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            window.addEventListener('load', function () {
+                document.getElementById('spinner').style.display = 'none';
+            });
+        });
+    </script>
 </body>
 </html>
