8f96f95afadeeee9347fc8ac01580181e6af69c0
faizulsoknan <mohdfaizul@uniti.edu.my>
2024-10-15 17:25:20 +0800

    update program orderBy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -59,7 +59,7 @@ class AdminController extends Controller
         $programs = DB::table('program')
             ->join('location', 'program.location_id', '=', 'location.id')
             ->select('program.id AS id', 'program.name AS program', 'location.name AS location', 'program.offered')
-            ->orderBy('program.location_id')
+            ->orderBy('program.location_id', 'program.id')
             ->get();
 
         $locations = DB::table('location')->get();
