2c0c826f336dacf8ed7316b357d38b9ca32a0304
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-03-12 12:32:11 +0800

    update embed layout

--- resources/views/layouts/embedded.blade.php
+++ resources/views/layouts/embedded.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'Kolej UNITI Port Dickson')</title>
 
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
@@ -23,127 +23,115 @@
         }
     </style>
     
-    <!-- Preconnect to Font Provider -->
+    {{-- <!-- Preconnect to Font Provider -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 
     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" as="style" onload="this.rel='stylesheet'">
     <noscript>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap">
-    </noscript>
+    </noscript> --}}
     
+    <!-- Fonts & Icons -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> --}}
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/kupd-style.css') }}">
 </head>
 <body class="bg-white">
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: darkslateblue;">
+        
+        <!--Top Start-->
+        {{-- <div class="top-class p-1">
+            <div class="container d-flex justify-content-between align-items-center">
+                <h7>UNITI Pilihan Terbaik Anda</h7>
+                <h7>No. Pendaftaran: DK036(N)</h7>
+            </div>
+        </div> --}}
+        <!--Top End-->
+
+        <!-- Nav Start -->
+        {{-- <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
           <div class="container">
-              <div class="row col-12">
-                  <div class="col-12 col-md-4">
-                      <label class="text-white">{{ __('NO. PERAKUAN PENDAFTARAN : DK036(N)') }}</label>
-                  </div>
-                  <div class="col-12 col-md-2">
-                      <label class="text-white"><i class="bi bi-telephone-fill"></i>&nbsp;{{ __('+606-6490350') }}</label>
-                  </div>
-                  <div class="col-12 col-md-4">
-                      <label class="text-white"><i class="bi bi-envelope-at-fill"></i>&nbsp;{{ __('info@uniti.edu.my') }}</label>
-                  </div>
-                  <div class="col-12 col-md-2 d-flex justify-content-md-end justify-content-start mt-md-0">
-                    <a href="https://uniti.edu.my" target="_blank" class="text-white mx-2"><i class="bi bi-globe-central-south-asia"></i></a>
-                    <a href="https://www.facebook.com/kolejunitiportdickson" target="_blank" class="text-white mx-2"><i class="bi bi-facebook"></i></a>
-                    <a href="https://www.instagram.com/kolejunitiportdickson/" target="_blank" class="text-white mx-2"><i class="bi bi-instagram"></i></a>
-                    <a href="https://www.youtube.com/KOLEJUNITIPORTDICKSON" target="_blank" class="text-white mx-2"><i class="bi bi-youtube"></i></a>
-                    <a href="https://www.tiktok.com/@kolejunitipd" target="_blank" class="text-white mx-2"><i class="bi bi-tiktok"></i></a>
-                  </div>
-              </div>
+            <!-- Logo -->
+            <a class="navbar-brand ms-0 me-auto" href="#">
+              <img src="{{ env('IMAGE_BASE_URL') }}/img/kupdonly.png" alt="Logo" style="max-width: 100%; height: 50px;">
+            </a>
+
+            <!-- Hamburger Toggler -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Menu Items -->
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul class="navbar-nav align-items-center gap-lg-4 gap-2">
+                <li class="nav-item">
+                  <a class="nav-link d-flex justify-content-end" href="{{ url('/') . (isset($source) ? '?source=' . $source : '') . (isset($ref) ? (isset($source) ? '&' : '?') . 'ref=' . $ref : '')}}">PILIH KAMPUS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link d-flex justify-content-end" href="{{ route('semak.permohonan.kupd') }}">SEMAK PERMOHONAN</a>
+                </li>
+              </ul>
+            </div>
+
           </div>
         </nav> --}}
-
-        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') . (request()->has('source') ? '?source=' . request('source') : '') . (request()->has('ref') ? (request()->has('source') ? '&' : '?') . 'ref=' . request('ref') : '') }}">
-                    <picture>
-                        <source srcset="https://ku-storage-object.ap-south-1.linodeobjects.com/urproject/images/logo/edaftar.webp" type="image/webp">
-                        <img src="https://ku-storage-object.ap-south-1.linodeobjects.com/urproject/images/logo/edaftar.png" alt="Logo" class="img-fluid" style="width: 150px; height: auto;">
-                    </picture>
-                </a>                
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item dropdown">
-                                <a href="{{ route('student.about') . (request()->has('source') ? '?source=' . request('source') : '') . (request()->has('ref') ? (request()->has('source') ? '&' : '?') . 'ref=' . request('ref') : '') }}" class="btn btn-success">Jom Masuk UNITI!</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a href="{{ route('student.about') . (request()->has('source') ? '?source=' . request('source') : '') . (request()->has('ref') ? (request()->has('source') ? '&' : '?') . 'ref=' . request('ref') : '') }}" class="btn btn-success">Jom Masuk UNITI!</a>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
+        <!-- Nav End -->
 
         <main class="py-3 bg-white">
             @yield('content')
         </main>
-        
-        {{-- <div class="container bg-white">
-            <footer>
-              <div class="row">
-                <div class="col-md-2 offset-md-5 mb-3">
-                  <h5>Pautan Segera</h5>
-                  <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="{{ route('student.about') . (request()->has('source') ? '?source=' . request('source') : '') . (request()->has('ref') ? (request()->has('source') ? '&' : '?') . 'ref=' . request('ref') : '') }}" class="nav-link p-0 text-body-secondary">Daftar Kemasukan</a>
-                    </li>
-                  </ul>
-                </div>
-                
-                <div class="col-md-2 mb-3">
-                    <h5>Info Kualiti</h5>
-                    <ul class="nav flex-column">
-                      <li class="nav-item mb-2"><a href="https://uniti.edu.my/polisi-kualiti/" class="nav-link p-0 text-body-secondary">Polisi Kualiti</a></li>
-                      <li class="nav-item mb-2"><a href="https://uniti.edu.my/objektif-kualiti/" class="nav-link p-0 text-body-secondary">Objektif Kualiti</a></li>
-                      <li class="nav-item mb-2"><a href="https://uniti.edu.my/sijil-dan-logo-iso/" class="nav-link p-0 text-body-secondary">Logo & Sijil ISO</a></li>
-                    </ul>
-                  </div>
 
-                <div class="col-md-3 mb-3">
-                    <form>
-                      <h5>UNITI, Pilihan Terbaik Anda</h5>
-                      <label class="mb-1">UNITI Village Persiaran UNITI</label><br>
-                      <label class="mb-1">Tanjung Agas, 71250 Port Dickson</label><br>
-                      <label class="mb-1">Negeri Sembilan</label><br>
-                      <label class="mb-1"><i class="bi bi-telephone-fill"></i>&nbsp;{{ __('+606-6490350') }}</label><br>
-                      <label class="mb-1"><i class="bi bi-envelope-at-fill"></i>&nbsp;{{ __('info@uniti.edu.my') }}</label>
-                    </form>
+        {{-- <footer class="footer bg-dark py-5">
+            <div class="container">
+              <div class="row">
+        
+                <div class="col-md-9 d-flex gap-3">
+                  <img src="{{ env('IMAGE_BASE_URL') }}/img/ku1.png" alt="Logo Kolej UNITI" class="me-3 mb-3 mb-md-0"
+                    style="max-width: 100px; height: auto;">
+                  <div>
+                    <h5 class="fw-bold mb-1 text-light">Kolej UNITI Port Dickson</h5>
+                    <p class="mb-1 text-light">UNITI Village, Persiaran UNITI, Tanjung Agas 71250 Port Dickson, Negeri Sembilan
+                    </p>
+                    <p class="mb-1 text-light"><i class="bi bi-telephone text-light"></i> +06-602 0303</p>
+                    <p class="mb-1 text-light"><i class="bi bi-envelope text-light"></i> info@uniti.edu.my</p>
                   </div>
-              </div>
-          
-              <div class="d-flex flex-column flex-sm-row justify-content-between py-2 border-top">
-                <p>&copy; Copyright Kolej UNITI 2024.</p>
-                <ul class="list-unstyled d-flex">
-                  <li class="ms-3"><a href="https://uniti.edu.my" target="_blank" class="text-dark"><i class="bi bi-globe-central-south-asia"></i></a></li>
-                  <li class="ms-3"><a href="https://www.facebook.com/kolejunitiportdickson" target="_blank" class="text-dark"><i class="bi bi-facebook"></i></a></li>
-                  <li class="ms-3"><a href="https://www.instagram.com/kolejunitiportdickson/" target="_blank" class="text-dark"><i class="bi bi-instagram"></i></a></li>
-                  <li class="ms-3"><a href="https://www.youtube.com/KOLEJUNITIPORTDICKSON" target="_blank" class="text-dark"><i class="bi bi-youtube"></i></a></li>
-                  <li class="ms-3"><a href="https://www.tiktok.com/@kolejunitipd" target="_blank" class="text-dark"><i class="bi bi-tiktok"></i></a></li>
-                </ul>
-              </div>
-            </footer>
-        </div> --}}
+                </div>
+        
+                <div
+                  class="col-md-3 d-flex flex-column align-items-center align-items-md-end justify-content-center text-md-end text-center mt-3 mt-md-0">
+                  <h5 class="fw-bold mb-3 text-light">Ikuti Kami</h5>
+                  <div class="d-flex gap-3 justify-content-center justify-content-md-end">
+                    <a href="https://www.facebook.com/kolejunitiportdickson" class="text-light"><i
+                        class="bi bi-facebook fs-1"></i></a>
+                    <a href="https://www.instagram.com/kolejunitiportdickson/" class="text-light"><i
+                        class="bi bi-instagram fs-1"></i></a>
+                    <a href="https://x.com/kolejuniti_pd" class="text-light"><i class="bi bi-twitter-x fs-1"></i></a>
+                    <a href="https://www.tiktok.com/@kolejunitipd" class="text-light"><i class="bi bi-tiktok fs-1"></i></a>
+                    <a href="https://www.youtube.com/@KolejUnitiPortDickson" class="text-light"><i
+                        class="bi bi-youtube fs-1"></i></a>
+                  </div>
+                </div>
+            </div><br>
+      
+            <hr class="border-light my-4">
+      
+            <p class="text-center text-light mb-0">Hak Cipta Terpelihara <span id="current-year"></span> © Kolej UNITI</p>
+          </div>
+        </footer> --}}
+
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
