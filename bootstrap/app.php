4982647962d112631bc25ecb4f74426d1bcf6789
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-03-26 12:55:43 +0800

    add cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Register CORS middleware globally
+        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
+        // Add any alias middleware if needed
         $middleware->alias([
             'user-access' => \App\Http\Middleware\MultiAuthUser::class,           
         ]);
