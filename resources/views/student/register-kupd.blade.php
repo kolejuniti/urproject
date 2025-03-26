7dbc260cb1d8efc0c85f9cc940a6663d5c54ccc8
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-03-26 13:11:40 +0800

    add csrf_token

--- resources/views/student/register-kupd.blade.php
+++ resources/views/student/register-kupd.blade.php
@@ -146,6 +146,7 @@
                 </div>
                 <div class="card-footer">
                     <div class="col-sm-12 text-center">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="hidden" name="source" id="source" value="{{ $source }}">
                         <button class="btn btn-primary" type="submit">Daftar</button>
                     </div>
