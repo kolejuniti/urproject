f6367df88862630c0b881238b2b9700a92a23f51
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-03-26 11:38:00 +0800

    enable route confirmation

--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,7 @@ Route::middleware(['auth', 'user-access:user'])->group(function() {
 Route::prefix('student')->group(function() {
     // Route::get('/register', [App\Http\Controllers\StudentController::class, 'index'])->name('student.register');
     // Route::post('/register', [App\Http\Controllers\StudentController::class, 'register'])->name('student.register.post');
-    // Route::get('/confirmation', [App\Http\Controllers\StudentController::class, 'confirmation'])->name('student.confirmation');
+    Route::get('/confirmation', [App\Http\Controllers\StudentController::class, 'confirmation'])->name('student.confirmation');
     // Route::get('/search', [App\Http\Controllers\StudentController::class, 'search'])->name('student.search');
     // Route::get('/location/{id}', [App\Http\Controllers\StudentController::class, 'location']);
     // Route::get('/offerletter', [App\Http\Controllers\StudentController::class, 'offerletter'])->name('student.offerletter');
