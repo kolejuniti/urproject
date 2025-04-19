43f5dfebd2d3ad21c1438bfe9b1b364ae322c10e
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-04-19 09:35:34 +0800

    add new database driver for cms kukb

--- config/database.php
+++ config/database.php
@@ -78,6 +78,25 @@ return [
             ]) : [],
         ],
 
+        'mysql3' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_3', '127.0.0.1'),
+            'port' => env('DB_PORT_3', '3306'),
+            'database' => env('DB_DATABASE_3', 'laravel'),
+            'username' => env('DB_USERNAME_3', 'root'),
+            'password' => env('DB_PASSWORD_3', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
