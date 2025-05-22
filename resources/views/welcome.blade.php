c466e473f66c09a1d90ecb7dae5a95325ecaf5da
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-05-22 12:56:19 +0800

    add google tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="jAW-nBBPmKdXh1IRqIAEZHzrj6t7QmoFX1IgsZMuqqE" />
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
