c164f1d69ce15075d3ce9cad96d52748918338cc
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-05-27 11:49:33 +0800

    add source on affiliate details

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1384,7 +1384,7 @@ class AdminController extends Controller
                             ->where('students.ic', '!=', '');
                     })
             ->whereBetween(DB::raw("CAST(students.created_at AS DATE)"), [$start_date, $end_date])
-            ->select('students.name', 'students.created_at', 'students.incentive', 'students.commission')
+            ->select('students.name', 'students.created_at', 'students.source', 'students.incentive', 'students.commission')
             ->orderByDesc('students.id');
 
                 if ($location == 3) {
