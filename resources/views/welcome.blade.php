d4e9b14a4629c61526c24f70d55f1e7efb29b8fe
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-06-04 09:36:34 +0800

    add conanical tag for page indexing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="google-site-verification" content="jAW-nBBPmKdXh1IRqIAEZHzrj6t7QmoFX1IgsZMuqqE" />
 
+  <!--Canonical Tag-->
+  <link rel="canonical" href="{{ url()->current() }}">
+
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
     integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
