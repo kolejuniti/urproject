0b74e72995f1bc2c8f3c5c55a5e5dcbffc812283
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-06-05 10:13:04 +0800

    fix canonical in the layout

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!--Canonical Tag-->
-    <link rel="canonical" href="{{ url()->current() }}">
+    <link rel="canonical" href="{{ $canonical ?? url()->current() }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
