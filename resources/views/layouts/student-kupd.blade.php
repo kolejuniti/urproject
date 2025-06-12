4942d3ab584b86fd80d4a690b80d24a768184e82
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-06-12 15:53:59 +0800

    add another google tag

--- resources/views/layouts/student-kupd.blade.php
+++ resources/views/layouts/student-kupd.blade.php
@@ -51,14 +51,16 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/kupd-style.css') }}">
   
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11015826304"></script>
+    <!-- Load gtag.js only ONCE -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B4BRS3VJS0"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', 'AW-11015826304');
+      // Configure BOTH tags using the same gtag instance
+      gtag('config', 'G-B4BRS3VJS0');      // Your GA4
+      gtag('config', 'AW-11015826304');    // Your Google Ads
     </script>
 </head>
 <body class="bg-white">
