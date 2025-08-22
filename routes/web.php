f5ce7bf15d148f78568ead153a2515be6e153e76
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-08-22 12:13:12 +0800

    add route to find slow query

--- routes/web.php
+++ routes/web.php
@@ -313,6 +313,6 @@ Route::get('/test-slow-query', function () {
 });
 
 Route::get('/other-slow-query', function () {
-    $results = DB::connection('mysql2')->table('student_subjek')->get();
+    $results = DB::connection('mysql2')->table('student_subjek')->paginate(50);
     return $results;
 });
