d1c0dd1cdf59fdd29b5f6cab8ed9dc9b38fd3687
hafiyaiman1998 <hafiyyaimann1998@gmail.com>
2026-01-22 16:13:05 +0800

    rename permohonan to data masuk

--- resources/views/user/application.blade.php
+++ resources/views/user/application.blade.php
@@ -57,8 +57,8 @@
                     <thead class="table-dark">
                         <tr>
                             <th>#</th>
-                            <th>Nama Pemohon</th>
-                            <th>Tarikh Permohonan</th>
+                            <th>Nama</th>
+                            <th>Tarikh Data Masuk</th>
                             <th>Status</th>
                             <th>Insentif</th>
                             <th>Tarikh Pendaftaran</th>
@@ -69,16 +69,16 @@
                     <tbody>
                         @foreach ($applicantsWithPrograms as $data)
                         @if ($data['applicant']->user_id !== null && $data['applicant']->register_at === null && in_array($data['applicant']->status_id, [1, 2, 3, 4, 5, 24, 26]))
-                            <tr class="table-danger">
-                        @elseif ($data['applicant']->user_id !== null && $data['applicant']->register_at === null && $data['applicant']->status_id === 19)
-                            <tr class="table-info">
-                        @elseif ($data['applicant']->user_id !== null && $data['applicant']->register_at === null)
-                            <tr class="table-warning">
-                        @elseif ($data['applicant']->user_id !== null && $data['applicant']->register_at !== null)
-                            <tr class="table-success">
-                        @else
-                            <tr>
-                        @endif
+                        <tr class="table-danger">
+                            @elseif ($data['applicant']->user_id !== null && $data['applicant']->register_at === null && $data['applicant']->status_id === 19)
+                        <tr class="table-info">
+                            @elseif ($data['applicant']->user_id !== null && $data['applicant']->register_at === null)
+                        <tr class="table-warning">
+                            @elseif ($data['applicant']->user_id !== null && $data['applicant']->register_at !== null)
+                        <tr class="table-success">
+                            @else
+                        <tr>
+                            @endif
                             <td>&nbsp;</td>
                             <td>
                                 <button type="button" class="btn btn-sm btn-link" data-bs-toggle="modal" data-bs-target="#modal{{ $data['applicant']->ic }}">{{ $data['applicant']->name }}</button>
@@ -93,67 +93,67 @@
                         <div class="modal fade" id="modal{{ $data['applicant']->ic }}" tabindex="-1" role="dialog" aria-labelledby="modalLabel{{ $data['applicant']->ic }}" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                                 <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="modalLabel{{ $data['applicant']->ic }}"></h5>
-                                </div>
-                                <div class="modal-body small">
-                                    <div class="col-md-12 col-sm-12 mb-3">
-                                        <label for="" class="fw-bold">Maklumat Pemohon</label>
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modalLabel{{ $data['applicant']->ic }}"></h5>
                                     </div>
-                                    <div class="row mb-2">
-                                        <div class="col-md-3 col-sm-3">
-                                            <label for="">Nama Penuh</label>
+                                    <div class="modal-body small">
+                                        <div class="col-md-12 col-sm-12 mb-3">
+                                            <label for="" class="fw-bold">Maklumat Data</label>
                                         </div>
-                                        <div class="col-md-9 col-sm-9">
-                                            <label for="name">{{ $data['applicant']->name }}</label>
+                                        <div class="row mb-2">
+                                            <div class="col-md-3 col-sm-3">
+                                                <label for="">Nama Penuh</label>
+                                            </div>
+                                            <div class="col-md-9 col-sm-9">
+                                                <label for="name">{{ $data['applicant']->name }}</label>
+                                            </div>
                                         </div>
+                                        <div class="row mb-2">
+                                            <div class="col-md-3 col-sm-3">
+                                                <label for="">Tarikh Data Masuk</label>
+                                            </div>
+                                            <div class="col-md-3 col-sm-3">
+                                                <label for="name">{{ \Carbon\Carbon::parse( $data['applicant']->created_at )->format('d-m-Y') }}</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12 col-sm-12 mb-3 mt-3">
+                                            <label for="" class="fw-bold">Maklumat Program</label>
+                                        </div>
+                                        <div class="row mb-2">
+                                            <div class="col-md-3 col-sm-3">
+                                                <label for="">Lokasi</label>
+                                            </div>
+                                            <div class="col-md-3 col-sm-3">
+                                                <label for="name">{{ $data['applicant']->location }}</label>
+                                            </div>
+                                        </div>
+                                        @foreach ($data['programs'] as $program)
+                                        <div class="row mb-2">
+                                            <div class="col-md-3 col-sm-3">
+                                                <label for="">Program Pilihan {{ $loop->iteration }}</label>
+                                            </div>
+                                            <div class="col-md-9 col-sm-9">
+                                                <label for="name">{{ $program->name }}</label>
+                                            </div>
+                                        </div>
+                                        @endforeach
+                                        @if ($data['applicant']->status !== null)
+                                        <div class="col-md-12 col-sm-12">
+                                            <hr>
+                                        </div>
+                                        <div class="row mb-2">
+                                            <div class="col-md-3 col-sm-3">
+                                                <label for="">Status Terkini</label>
+                                            </div>
+                                            <div class="col-md-9 col-sm-9">
+                                                <label for="" class="text-uppercase">{{ $data['applicant']->status }}</label>
+                                            </div>
+                                        </div>
+                                        @endif
                                     </div>
-                                    <div class="row mb-2">
-                                        <div class="col-md-3 col-sm-3">
-                                            <label for="">Tarikh Permohonan</label>
-                                        </div>
-                                        <div class="col-md-3 col-sm-3">
-                                            <label for="name">{{ \Carbon\Carbon::parse( $data['applicant']->created_at )->format('d-m-Y') }}</label>
-                                        </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">Close</button>
                                     </div>
-                                    <div class="col-md-12 col-sm-12 mb-3 mt-3">
-                                        <label for="" class="fw-bold">Program Yang Dipohon</label>
-                                    </div>
-                                    <div class="row mb-2">
-                                        <div class="col-md-3 col-sm-3">
-                                            <label for="">Lokasi</label>
-                                        </div>
-                                        <div class="col-md-3 col-sm-3">
-                                            <label for="name">{{ $data['applicant']->location }}</label>
-                                        </div>
-                                    </div>
-                                    @foreach ($data['programs'] as $program)
-                                    <div class="row mb-2">
-                                        <div class="col-md-3 col-sm-3">
-                                            <label for="">Program Pilihan {{ $loop->iteration }}</label>
-                                        </div>
-                                        <div class="col-md-9 col-sm-9">
-                                            <label for="name">{{ $program->name }}</label>
-                                        </div>
-                                    </div>
-                                    @endforeach
-                                    @if ($data['applicant']->status !== null)
-                                    <div class="col-md-12 col-sm-12">
-                                        <hr>
-                                    </div>
-                                    <div class="row mb-2">
-                                        <div class="col-md-3 col-sm-3">
-                                            <label for="">Status Permohonan</label>
-                                        </div>
-                                        <div class="col-md-9 col-sm-9">
-                                            <label for="" class="text-uppercase">{{ $data['applicant']->status }}</label>
-                                        </div>
-                                    </div>                                            
-                                    @endif
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">Close</button>
-                                </div>
                                 </div>
                             </div>
                         </div>
@@ -170,35 +170,32 @@
 <script>
     $(document).ready(function() {
         var t = $('#myTable').DataTable({
-            columnDefs: [
-                {
-                    targets: ['_all'],
-                    className: 'dt-head-center'
-                }
-            ],
+            columnDefs: [{
+                targets: ['_all'],
+                className: 'dt-head-center'
+            }],
             layout: {
                 top1Start: {
                     div: {
-                        html: '<h2>Senarai Permohonan</h2>'
+                        html: '<h2>Senarai Data Masuk</h2>'
                     }
                 },
                 top1End: {
-                    buttons: [
-                        {
+                    buttons: [{
                             extend: 'copy',
-                            title: 'Senarai Permohonan'
+                            title: 'Senarai Data Masuk'
                         },
                         {
                             extend: 'excelHtml5',
-                            title: 'Senarai Permohonan'
+                            title: 'Senarai Data Masuk'
                         },
                         {
                             extend: 'pdfHtml5',
-                            title: 'Senarai Permohonan'
+                            title: 'Senarai Data Masuk'
                         },
                         {
                             extend: 'print',
-                            title: 'Senarai Permohonan'
+                            title: 'Senarai Data Masuk'
                         }
                     ]
                 },
@@ -208,10 +205,13 @@
                 bottomEnd: 'paging'
             }
         });
-        t.on('order.dt search.dt', function () {
+        t.on('order.dt search.dt', function() {
             let i = 1;
-        
-            t.cells(null, 0, { search: 'applied', order: 'applied' }).every(function (cell) {
+
+            t.cells(null, 0, {
+                search: 'applied',
+                order: 'applied'
+            }).every(function(cell) {
                 this.data(i++);
             });
         }).draw();
@@ -222,14 +222,14 @@
         var copyText = document.getElementById("referral_url");
         copyText.select();
         document.execCommand("copy");
-        
+
         var copyBtn = document.getElementById("copy-btn");
         var originalText = copyBtn.innerHTML;
         copyBtn.innerHTML = '<i class="bi bi-check-circle"></i>';
-        
+
         var copyAlert = document.getElementById("copy-alert");
         copyAlert.style.display = "block";
-        
+
         setTimeout(function() {
             copyBtn.innerHTML = originalText;
             copyAlert.style.display = "none";
@@ -255,57 +255,57 @@
         window.open("https://t.me/share/url?url=" + encodeURIComponent(document.getElementById("referral_url").value) + "&text=" + encodeURIComponent(text));
     }
 
-// Make QR code clickable to download
-document.addEventListener('DOMContentLoaded', function() {
-    var qrContainer = document.querySelector('.qr-container');
-    var qrOverlay = document.querySelector('.qr-overlay');
-    
-    if (qrContainer) {
-        qrContainer.addEventListener('mouseenter', function() {
-            qrOverlay.style.opacity = '1';
-        });
-        
-        qrContainer.addEventListener('mouseleave', function() {
-            qrOverlay.style.opacity = '0';
-        });
-        
-        qrContainer.addEventListener('click', function() {
-            // Get the SVG element
-            var svg = qrContainer.querySelector('svg');
-            if (!svg) {
-                console.error('SVG not found in container');
-                return;
-            }
-            
-            // Create a canvas element
-            var canvas = document.createElement('canvas');
-            var context = canvas.getContext('2d');
-            
-            // Set canvas dimensions to match SVG
-            canvas.width = svg.clientWidth || 200;
-            canvas.height = svg.clientHeight || 200;
-            
-            // Create an image from the SVG
-            var image = new Image();
-            var svgData = new XMLSerializer().serializeToString(svg);
-            var svgURL = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgData);
-            
-            image.onload = function() {
-                // Draw the image on the canvas
-                context.drawImage(image, 0, 0);
-                
-                // Create download link
-                var downloadLink = document.createElement('a');
-                downloadLink.download = 'UNITI-QR-Code.png';
-                downloadLink.href = canvas.toDataURL('image/png');
-                document.body.appendChild(downloadLink);
-                downloadLink.click();
-                document.body.removeChild(downloadLink);
-            };
-            
-            image.src = svgURL;
-        });
-    }
-});
+    // Make QR code clickable to download
+    document.addEventListener('DOMContentLoaded', function() {
+        var qrContainer = document.querySelector('.qr-container');
+        var qrOverlay = document.querySelector('.qr-overlay');
+
+        if (qrContainer) {
+            qrContainer.addEventListener('mouseenter', function() {
+                qrOverlay.style.opacity = '1';
+            });
+
+            qrContainer.addEventListener('mouseleave', function() {
+                qrOverlay.style.opacity = '0';
+            });
+
+            qrContainer.addEventListener('click', function() {
+                // Get the SVG element
+                var svg = qrContainer.querySelector('svg');
+                if (!svg) {
+                    console.error('SVG not found in container');
+                    return;
+                }
+
+                // Create a canvas element
+                var canvas = document.createElement('canvas');
+                var context = canvas.getContext('2d');
+
+                // Set canvas dimensions to match SVG
+                canvas.width = svg.clientWidth || 200;
+                canvas.height = svg.clientHeight || 200;
+
+                // Create an image from the SVG
+                var image = new Image();
+                var svgData = new XMLSerializer().serializeToString(svg);
+                var svgURL = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgData);
+
+                image.onload = function() {
+                    // Draw the image on the canvas
+                    context.drawImage(image, 0, 0);
+
+                    // Create download link
+                    var downloadLink = document.createElement('a');
+                    downloadLink.download = 'UNITI-QR-Code.png';
+                    downloadLink.href = canvas.toDataURL('image/png');
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                    document.body.removeChild(downloadLink);
+                };
+
+                image.src = svgURL;
+            });
+        }
+    });
 </script>
 @endsection
