31aa8cf557832ab02f5d08d7f581f7218ea13dd5
faizulsoknan <faizulsoknan@gmail.com>
2026-02-19 12:36:27 +0800

    fix: move determineSourceFromReferrer to helpers.php to fix route cache error

--- routes/web.php
+++ routes/web.php
@@ -95,38 +95,7 @@ Route::get('/image-proxy', function (Request $request) {
 //     return view('welcome', compact('ref', 'source')); // Or any other public view
 // });
 
-if (!function_exists('determineSourceFromReferrer')) {
-    function determineSourceFromReferrer($referrer)
-    {
-        $referrer = strtolower($referrer); // case insensitive
 
-        if ($referrer === 'other') {
-            return 'e-Daftar';
-        } elseif ($referrer === 'tiktok') {
-            return 'tiktok';
-        }
-
-        if (strpos($referrer, 'facebook.com') !== false) {
-            return 'facebook';
-        } elseif (strpos($referrer, 'whatsapp.com') !== false) {
-            return 'whatsapp';
-        } elseif (strpos($referrer, 'tiktok.com') !== false || strpos($referrer, 'pangleglobal.com') !== false || strpos($referrer, 'pangle.io') !== false) {
-            return 'tiktok';
-        } elseif (strpos($referrer, 'instagram.com') !== false) {
-            return 'instagram';
-        } elseif (strpos($referrer, 'edaftarkolej.uniticms.edu.my') !== false) {
-            return 'e-Daftar';
-        } elseif (strpos($referrer, 'uniti.edu.my') !== false) {
-            return 'website';
-        } elseif (strpos($referrer, 'google.com') !== false || strpos($referrer, 'google.com.my') !== false) {
-            return 'google';
-        } elseif (strpos($referrer, 'youtube.com') !== false) {
-            return 'youtube';
-        }
-
-        return 'e-Daftar'; // default fallback
-    }
-}
 
 Route::get('/', function (Request $request) {
     if (Auth::check()) {
